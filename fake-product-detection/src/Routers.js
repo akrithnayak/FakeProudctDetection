ab66d03090686b44b56b38e58d385743d0f9913e
BhavishBSShetty <bhavishbsshetty200@gmail.com>
2021-12-04 14:30:05 +0530

    added home page

--- fake-product-detection/src/Routers.js
+++ fake-product-detection/src/Routers.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./components/Login";
 import Navbar from "./components/Navbar";
 import SignUp from "./components/SignUp";
+import Home from "./components/Home";
 
 function Routers() {
   return (
@@ -10,6 +11,7 @@ function Routers() {
       <div className="App">
         <Navbar />
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route exact path="/" element={<Navigate to="/sign-in" />} />
           <Route path="/sign-in" element={<Login />} />
           <Route exact path="/sign-up" element={<SignUp />} />
